c44ccb044423a97f003380c3466f426d145da9f4
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-04-07 19:15:14 +0200

    Add a StaticPages controller

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe StaticPagesController do
+
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+  end
+
+end
