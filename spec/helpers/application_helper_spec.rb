7c4426df364feb79a291c2c16e776f468830df7b
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-04-16 00:22:03 +0200

    Finish refactoring tests

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,17 @@
+describe "Application Helper" do
+  
+  describe "full title" do
+    
+    it "should include the page name" do
+      full_title("foo").should =~ /foo/
+    end
+
+    it "should include the base name" do
+      full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
+    end
+
+    it "should not include a bar for the home page" do
+      full_title("").should_not =~ /\|/
+    end
+  end
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,38 +1,89 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static Pages" do
 
-  # let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   # full_title method referenced in spec/support/utilties.rb
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
+  shared_examples_for "all layout link verifications" do
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
+  describe "layout" do
+    before { visit root_path }
+    
+    context "About link" do
+      before { click_link "About" }
+      let(:page_title) { 'About Us' }
+      
+      it_should_behave_like "all layout link verifications"
+    end
+
+    context "Help link" do
+      before { click_link "Help" }
+      let(:page_title) { 'Help' }
+
+      it_should_behave_like "all layout link verifications"
+    end
+
+    context "Contact link" do
+      before { click_link "Contact" }
+      let(:page_title) { 'Contact' }
+
+      it_should_behave_like "all layout link verifications"
+    end
+
+    context "Home link" do
+      before { click_link "Home" }
+      let(:page_title) { '' }
+
+      it_should_behave_like "all layout link verifications"
+    end
+
+    context "Sign up link" do
+      before { click_link "Sign up now!" }
+      let(:page_title) { 'Sign Up' }
+
+      it_should_behave_like "all layout link verifications"
+    end
+  end
   
   describe "Home page" do
     before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1',        text: 'Sample App') }
-    it { should have_selector('title',     text: full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_selector('title', text: '| Home') }
   end
 
   describe "Help Page" do
     before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
 
-  	it { should have_selector('h1',    text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About Page" do
     before { visit about_path }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
 
-  	it { should have_selector('h1',    text: 'About Us') }
-    it { should have_selector('title', text: full_title('About Us')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact Page" do
     before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    it_should_behave_like "all static pages"
   end
 
 end
