084c0f7f08e2a1fa20e2335fe684569a08db5d8e
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-05-03 21:32:33 +0200

    Change location of a comment

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
-  # full_title method referenced in spec/support/utilties.rb
   subject { page }
 
+  # full_title method referenced in spec/support/utilties.rb
   shared_examples_for "all static pages" do
     it { should have_selector('h1',    text: heading) }
     it { should have_selector('title', text: full_title(page_title)) }
