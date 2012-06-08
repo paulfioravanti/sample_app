b163c59c6b69b31ded2332020f11b057cd5a22c8
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-06-08 18:11:13 +0200

    Add Heroku setting for asset precompilation

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku settings
+    config.assets.initialize_on_precompile = false
   end
 end
