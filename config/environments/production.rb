3e35f87bc4392dec5c7b804e8e6714936364ddf5
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-06-29 22:24:45 +0200

    New attempt to get Heroku to compile assets properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ SampleApp::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( #{Rails.root}/lib/assets/javascripts/timeago/jquery.timeago.ja.js )
+  config.assets.precompile += %w( locales/jquery.timeago.it.js jquery.timeago.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,5 +1,5 @@
-require 'localeapp/rails'
+# require 'localeapp/rails'
 
-Localeapp.configure do |config|
-  config.api_key = 'Z2i561HQg8QRPkgRxoyhBbW0af6x2Ez4wbw0VDb8Wo5abbQDiC'
-end
+# Localeapp.configure do |config|
+#   config.api_key = 'Z2i561HQg8QRPkgRxoyhBbW0af6x2Ez4wbw0VDb8Wo5abbQDiC'
+# end
