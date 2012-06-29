b120d8810a5feff1526c258c3eef726d045b3f89
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-06-29 23:17:10 +0200

    Move timeago ja file into its own folder

--- config/initializers/timeago.rb
+++ config/initializers/timeago.rb
@@ -2,4 +2,4 @@ Rails::Timeago.default_options limit: proc { 20.days.ago }, nojs: true
 Rails::Timeago.locales = [:en, :it, :ja]
 Rails::Timeago.map_locale "en", "jquery.timeago.js"
 Rails::Timeago.map_locale "it", "locales/jquery.timeago.it.js"
-Rails::Timeago.map_locale "ja", "jquery.timeago.ja.js"
+Rails::Timeago.map_locale "ja", "timeago/jquery.timeago.ja.js"
