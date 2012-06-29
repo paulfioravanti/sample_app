3991c20a168f4d8fc3c6632a7fb4a0861693243c
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-06-29 16:20:00 +0200

    Override all locales to get timeago working locally, and precompile them to get Heroku working

--- config/initializers/timeago.rb
+++ config/initializers/timeago.rb
@@ -1,5 +1,5 @@
 Rails::Timeago.default_options limit: proc { 20.days.ago }, nojs: true
 Rails::Timeago.locales = [:en, :it, :ja]
-Rails::Timeago.map_locale "en", "better/locales/en.js"
-Rails::Timeago.map_locale "it", "better/locales/it.js"
-Rails::Timeago.map_locale "ja", "better/locales/ja.js"
+Rails::Timeago.map_locale "en", "timeago/jquery.timeago.en.js"
+Rails::Timeago.map_locale "it", "timeago/jquery.timeago.it.js"
+Rails::Timeago.map_locale "ja", "timeago/jquery.timeago.ja.js"
