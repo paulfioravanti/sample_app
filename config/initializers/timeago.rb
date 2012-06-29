8f78ab14743f553a4771b4229433c697c5c6ae04
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-06-29 22:40:50 +0200

    Change ja locale setting

--- config/initializers/timeago.rb
+++ config/initializers/timeago.rb
@@ -1,4 +1,4 @@
 Rails::Timeago.default_options limit: proc { 20.days.ago }, nojs: true
 Rails::Timeago.locales = [:en, :it, :ja]
 Rails::Timeago.map_locale "en", "jquery.timeago.js"
-Rails::Timeago.map_locale "ja", "locales/jquery.timeago.ja.js"
+Rails::Timeago.map_locale "ja", "timeago/jquery.timeago.ja.js"
