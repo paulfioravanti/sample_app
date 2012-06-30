506cf4353d87592d8249191e942076ae5ba2118a
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-06-30 17:57:18 +0200

    app/assets/javascripts/timeago/jquery.timeago.ja.js

---
+++ spec/routing/routing_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+
+describe "Routes" do
+
+  describe "locale scoped paths" do
+    I18n.available_locales.each do |locale|
+
+      describe "routing" do
+        it "should route /:locale to the root path" do
+          get("/#{locale.to_s}").
+            should route_to("static_pages#home", locale: locale.to_s)
+        end
+      end
+
+      describe "redirecting", type: :request do
+
+        subject { response }
+
+        context "fake paths" do
+          let(:fake_path) { "fake_path" }
+
+          before { get "/#{locale.to_s}/#{fake_path}" }
+          it { should redirect_to(root_path(locale)) }
+        end
+      end
+
+    end
+  end
+
+  describe "non-locale scoped paths" do
+
+    describe "redirecting", type: :request do
+
+      subject { response }
+
+      context "no path given" do
+        before { get "/" }
+        it { should redirect_to(root_path(I18n.default_locale)) }
+      end
+
+      context "a valid action" do
+        let(:action) { "about" }
+        let!(:default_locale_action_path) { about_path(I18n.default_locale) }
+
+        context "with a valid but unsupported locale" do
+          let(:invalid_locale) { "fr" }
+
+          before { get "/#{invalid_locale}/#{action}" }
+          it { should redirect_to(default_locale_action_path) }
+        end
+
+        context "with invalid information for the locale" do
+          let(:invalid_info) { "invalid" }
+
+          before { get "/#{invalid_info}/#{action}" }
+          it { should redirect_to(default_locale_action_path) }
+        end
+
+        context "with no locale information" do
+          before { get "/#{action}" }
+          it { should redirect_to(default_locale_action_path) }
+        end
+      end
+
+      context "invalid information" do
+        let(:invalid_info) { "invalid" }
+
+        before { get "/#{invalid_info}" }
+        it { should redirect_to("/#{I18n.default_locale}/#{invalid_info}") }
+        # This will then get caught by the "redirecting fake paths" condition
+        # and hence be redirected to root_path with I18n.default_locale
+      end
+    end
+  end
+end
