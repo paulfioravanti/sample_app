0b88bdc010d259709f1a296fe6de8879d8e804aa
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-09-01 22:39:24 +0930

    Fix undefined method 'root' for Rails:Module in Travis-CI attempt 4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,17 +15,79 @@ Spork.prefork do
   end
 
   require 'rails/application'
-  Dir[Rails.root.join("config/application")].each {|f| require f}
+  # require Rails.root.join("config/application")
   # require File.expand_path("../../config/application", __FILE__)
 
   ENV["RAILS_ENV"] ||= 'test'
   # require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
-  require 'rspec/autorun'
+  # require 'rspec/rails'
+  # require 'rspec/autorun'
 
   # # Requires supporting ruby files with custom matchers and macros, etc,
   # # in spec/support/ and its subdirectories.
   # Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  # RSpec.configure do |config|
+  #   # ## Mock Framework
+  #   #
+  #   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #   #
+  #   # config.mock_with :mocha
+  #   # config.mock_with :flexmock
+  #   # config.mock_with :rr
+  #   config.mock_with :rspec
+
+  #   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  #   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  #   # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  #   # examples within a transaction, remove the following line or assign false
+  #   # instead of true.
+  #   # config.use_transactional_fixtures = true
+  #   config.use_transactional_fixtures = false
+
+  #   config.before :each do
+  #     if Capybara.current_driver == :rack_test
+  #       DatabaseCleaner.strategy = :transaction
+  #     else
+  #       DatabaseCleaner.strategy = :truncation
+  #     end
+  #     DatabaseCleaner.start
+  #   end
+
+  #   config.after do
+  #     DatabaseCleaner.clean
+  #   end
+
+  #   # If true, the base class of anonymous controllers will be inferred
+  #   # automatically. This will be the default behavior in future versions of
+  #   # rspec-rails.
+  #   config.infer_base_class_for_anonymous_controllers = false
+  # end
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+  if ENV['DRB']
+    require 'simplecov'
+    SimpleCov.start 'rails'
+    require Rails.root.join("config/application")
+    SampleApp::Application.initialize!
+    class SampleApp::Application
+      def initialize!; end
+    end
+  end
+
+  unless ENV['DRB']
+    require File.expand_path("../../config/environment", __FILE__)
+  end
+
+  require 'rspec/rails'
+  require 'rspec/autorun'
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   RSpec.configure do |config|
     # ## Mock Framework
@@ -64,22 +126,6 @@ Spork.prefork do
     # rspec-rails.
     config.infer_base_class_for_anonymous_controllers = false
   end
-end
-
-Spork.each_run do
-  # This code will be run each time you run your specs.
-  if ENV['DRB']
-    require 'simplecov'
-    SimpleCov.start 'rails'
-    SampleApp::Application.initialize!
-    class SampleApp::Application
-      def initialize!; end
-    end
-  end
-
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 end
 
