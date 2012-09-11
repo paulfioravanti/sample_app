e3f50c908bde7bb325f894d117696bc68476500a
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-09-11 13:53:40 +0930

    Move staging configuration to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,10 @@
 SampleApp::Application.configure do
+
+  config.middleware.insert_after(::Rack::Lock,
+                                 "::Rack::Auth::Basic",
+                                 "Staging") do |u, p|
+    [u, p] == [ENV['SITE_USERNAME'], ENV['SITE_PASSWORD']]
+  end
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
--- config/environments/staging.rb
+++
@@ -1,9 +0,0 @@
-SampleApp::Application.configure do
-  config.middleware.insert_after(::Rack::Lock,
-                                 "::Rack::Auth::Basic",
-                                 "Staging") do |u, p|
-    [u, p] == [ENV['SITE_USERNAME'], ENV['SITE_PASSWORD']]
-
-
-  end
-end
