15d1e5e8dff7d2767b4701feaf7ae23fe946eff7
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-10-18 22:01:31 +1030

    Fix bug in associated microposts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -142,8 +142,9 @@ describe User do
     end
 
     it "destroys dependent microposts" do
-      microposts = user.microposts
+      microposts = user.microposts.dup
       user.destroy
+      microposts.should_not be_empty
       microposts.each do |micropost|
         Micropost.find_by_id(micropost.id).should be_nil
       end
