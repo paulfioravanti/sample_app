3152290fef04f47dc959faec7cb93089c9722cbf
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-11-14 17:08:42 +1030

    Clean assets

--- public/assets/timeago/jquery.timeago.ja-ea035d3d88df287e91e552a65da4b609.js
+++
@@ -1,2 +0,0 @@
-// Japanese
-jQuery.timeago.settings.strings={prefixAgo:"",prefixFromNow:"今より",suffixAgo:"前",suffixFromNow:"後",seconds:"数秒",minute:"約一分",minutes:"%d分",hour:"約1時間",hours:"約%d時間",day:"1日",days:"%d日ほど",month:"約1ヶ月",months:"%dヶ月ほど",year:"約1年間",years:"%d年間",wordSeparator:""};
