21765553536f2042e5cad290d982556210e6fb82
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-12-06 14:26:26 +1030

    Add i18n to tests

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe ApplicationHelper do
   describe "#full_title" do
-    context "with page name" do
-      I18n.available_locales.each do |locale|
+    I18n.available_locales.each do |locale|
+      context "with page name" do
         let(:page_name) { 'foo' }
         let(:base_name) { t('layouts.application.base_title') }
 
-        before { I18n.locale = locale }
-
         subject { full_title(page_name) }
+
+        before { I18n.locale = locale }
 
         it { should =~ /#{page_name}/ }
         it { should =~ /^#{base_name}/ }
@@ -29,15 +29,21 @@ describe ApplicationHelper do
     it { should == link }
   end
 
-  describe "#locale_languages" do
-    let(:language_labels) do
-      [
-        { label: t('locale_selector.en'), locale: 'en' },
-        { label: t('locale_selector.it'), locale: 'it' },
-        { label: t('locale_selector.ja'), locale: 'ja' }
-      ]
+  I18n.available_locales.each do |locale|
+    describe "#locale_languages" do
+      let(:language_labels) do
+        [
+          { label: t('locale_selector.en'), locale: 'en' },
+          { label: t('locale_selector.it'), locale: 'it' },
+          { label: t('locale_selector.ja'), locale: 'ja' }
+        ]
+      end
+
+      subject { locale_languages }
+
+      before { I18n.locale = locale }
+
+      it { should ==  language_labels }
     end
-    subject { locale_languages }
-    it { should ==  language_labels }
   end
 end
