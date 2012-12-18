5a2a0032f97ae86ddaf1856fff8a7527c29b9710
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-12-18 16:44:52 +1030

    Add monkey patch to get Rails Footnotes to recognize Haml templates

--- config/initializers/rails_footnotes.rb
+++ config/initializers/rails_footnotes.rb
@@ -1,5 +1,13 @@
 if defined?(Footnotes) && Rails.env.development?
   Footnotes.run! # first of all
+  Footnotes::Notes::LogNote::LoggingExtensions.module_eval do
+    def add(*args, &block)
+      super
+      logged_message = args[2] + "\n"
+      Footnotes::Notes::LogNote.log(logged_message)
+      logged_message
+    end
+  end
 
   # ... other init code
   Footnotes::Filter.prefix = 'subl://open?url=file://%s&line=%d&column=%d'
