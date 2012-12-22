be2059443d87ba11b7ab472117cef1a99cfa8a6a
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-12-23 10:07:17 +1030

    Add specs for all named custom routes

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -9,6 +9,36 @@ describe "Routing" do
         subject { get("/#{locale.to_s}") }
         it { should route_to("static_pages#home", locale: locale.to_s) }
       end
+
+      describe "/signup routing" do
+        subject { get("/#{locale.to_s}/signup") }
+        it { should route_to("users#new", locale: locale.to_s) }
+      end
+
+      describe "/signin routing" do
+        subject { get("/#{locale.to_s}/signin") }
+        it { should route_to("sessions#new", locale: locale.to_s) }
+      end
+
+      describe "/signout routing" do
+        subject { delete("/#{locale.to_s}/signout") }
+        it { should route_to("sessions#destroy", locale: locale.to_s) }
+      end
+
+      describe "/help routing" do
+        subject { get("/#{locale.to_s}/help") }
+        it { should route_to("static_pages#help", locale: locale.to_s) }
+      end
+
+      describe "/about routing" do
+        subject { get("/#{locale.to_s}/about") }
+        it { should route_to("static_pages#about", locale: locale.to_s) }
+      end
+
+      describe "/contact routing" do
+        subject { get("/#{locale.to_s}/contact") }
+        it { should route_to("static_pages#contact", locale: locale.to_s) }
+      end
     end
   end
 end
