c188ca49c7a0d1e84673eb1307e186dbf250e9e8
Paul Fioravanti <paul.fioravanti@gmail.com>
2012-12-24 13:31:54 +1030

    Comment out code that trips up Rails Breakman for now.  When the service upgrades to 1.9.0, then can swap the code back in

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -26,9 +26,13 @@ class Micropost < ActiveRecord::Base
   default_scope order: 'microposts.created_at DESC'
 
   def self.from_users_followed_by(user)
-    followed_user_ids = Relationship.select(:followed_id).
-                        where("follower_id = :user_id").
-                        to_sql
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    # When Rails Brakeman service upgrades to 1.9.0, this
+    # code can be swapped in for the above
+    # followed_user_ids = Relationship.select(:followed_id).
+    #                     where("follower_id = :user_id").
+    #                     to_sql
     where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
           user_id: user.id)
   end
