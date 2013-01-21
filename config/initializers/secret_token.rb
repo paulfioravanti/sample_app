5fa4fddc47187a968491373eafb6988b0ba16e6d
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-01-22 10:00:17 +1030

    Refactor secret token out of config for production on Heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'aed3149b0d601995f23119eaefc43ee7ead7ed3cc2074c2d045b737e2602bf3aa01c4f317524f05b579241211010a932867f04c563482273134cfb52e50c833c'
+
+if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
+  raise 'SECRET_TOKEN environment variable must be set!'
+end
+
+SampleApp::Application.config.secret_token =
+  ENV['SECRET_TOKEN'] || 'aed3149b0d601995f23119eaefc43ee7ead7ed3cc2074c2d045b737e2602bf3aa01c4f317524f05b579241211010a932867f04c563482273134cfb52e50c833c'
