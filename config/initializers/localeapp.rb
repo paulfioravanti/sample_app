a573aef6b3a071780866cb1efcf8e9ac558557db
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-01-22 14:39:30 +1030

    Minor syntax change

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,7 +1,7 @@
-unless ENV["LOCALE_API_KEY"].blank?
+unless ENV['LOCALE_API_KEY'].blank?
   require 'localeapp/rails'
 
   Localeapp.configure do |config|
-    config.api_key = ENV["LOCALE_API_KEY"]
+    config.api_key = ENV['LOCALE_API_KEY']
   end
 end
