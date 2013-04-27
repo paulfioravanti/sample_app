532337cb8b939c90ec5a61fbdfc328f5947f671c
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-04-27 10:56:53 +0930

    Turn off random ordered tests as it interferes directly with locale-dependent tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,7 +38,7 @@ Spork.prefork do
 
   RSpec.configure do |config|
     config.mock_with :rspec
-    config.order = "random"
+    # config.order = "random"
 
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
